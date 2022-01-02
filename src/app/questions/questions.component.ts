729741818df7721afc91df544a918c9ef71aa4c8
Gabriela Fraczek <gfraczek1999@gmail.com>
2022-01-02 15:05:53 +0100

    questions list

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
-  styleUrls: ['./questions.component.scss']
+  styleUrls: ['./questions.component.scss'],
 })
-export class QuestionsComponent implements OnInit {
+export class QuestionsComponent {
+  elements = [
+    {
+      name: 'Komputerowe przetwarzanie obrazu',
+      study: 'Informatyka',
+      specialization: 'Grafika komputerowa i Multimedia',
+      questions: [
+        'Cechy dźwięku: wysokość, barwa, głośność ( także inne: „twardość”, „jasność” „klarowność” itp.) oraz ich związek z parametrami fizycznymi.',
+        'Helmholtza teoria percepcji wysokości oraz wrażenia konsonansu i dysonansu.',
+        'Pochodzenie i rodzaje skal muzycznych. Strój „naturalny” i równomiernie temperowany.',
+        'Formy muzyki europejskiej i ich związek z fizycznymi własnościami dźwięku: organum, polifonia imitacyjna (fuga, a interwał kwinty), forma okresowa, allegro sonatowe.',
+      ],
+      expanded: false,
+    },
+    {
+      name: 'Komputerowe przetwarzanie obrazu',
+      study: 'Informatyka',
+      specialization: 'Grafika komputerowa i Multimedia',
+      questions: [
+        'Cechy dźwięku: wysokość, barwa, głośność ( także inne: „twardość”, „jasność” „klarowność” itp.) oraz ich związek z parametrami fizycznymi.',
+        'Helmholtza teoria percepcji wysokości oraz wrażenia konsonansu i dysonansu.',
+        'Pochodzenie i rodzaje skal muzycznych. Strój „naturalny” i równomiernie temperowany.',
+        'Formy muzyki europejskiej i ich związek z fizycznymi własnościami dźwięku: organum, polifonia imitacyjna (fuga, a interwał kwinty), forma okresowa, allegro sonatowe.',
+      ],
+      expanded: false,
+    },
+    {
+      name: 'Komputerowe przetwarzanie obrazu',
+      study: 'Informatyka',
+      specialization: 'Grafika komputerowa i Multimedia',
+      questions: [
+        'Cechy dźwięku: wysokość, barwa, głośność ( także inne: „twardość”, „jasność” „klarowność” itp.) oraz ich związek z parametrami fizycznymi.',
+        'Helmholtza teoria percepcji wysokości oraz wrażenia konsonansu i dysonansu.',
+        'Pochodzenie i rodzaje skal muzycznych. Strój „naturalny” i równomiernie temperowany.',
+        'Formy muzyki europejskiej i ich związek z fizycznymi własnościami dźwięku: organum, polifonia imitacyjna (fuga, a interwał kwinty), forma okresowa, allegro sonatowe.',
+      ],
+      expanded: false,
+    },
+    {
+      name: 'Komputerowe przetwarzanie obrazu',
+      study: 'Informatyka',
+      specialization: 'Grafika komputerowa i Multimedia',
+      questions: [
+        'Cechy dźwięku: wysokość, barwa, głośność ( także inne: „twardość”, „jasność” „klarowność” itp.) oraz ich związek z parametrami fizycznymi.',
+        'Helmholtza teoria percepcji wysokości oraz wrażenia konsonansu i dysonansu.',
+        'Pochodzenie i rodzaje skal muzycznych. Strój „naturalny” i równomiernie temperowany.',
+        'Formy muzyki europejskiej i ich związek z fizycznymi własnościami dźwięku: organum, polifonia imitacyjna (fuga, a interwał kwinty), forma okresowa, allegro sonatowe.',
+      ],
+      expanded: false,
+    },
+    {
+      name: 'Komputerowe przetwarzanie obrazu',
+      study: 'Informatyka',
+      specialization: 'Grafika komputerowa i Multimedia',
+      questions: [
+        'Cechy dźwięku: wysokość, barwa, głośność ( także inne: „twardość”, „jasność” „klarowność” itp.) oraz ich związek z parametrami fizycznymi.',
+        'Helmholtza teoria percepcji wysokości oraz wrażenia konsonansu i dysonansu.',
+        'Pochodzenie i rodzaje skal muzycznych. Strój „naturalny” i równomiernie temperowany.',
+        'Formy muzyki europejskiej i ich związek z fizycznymi własnościami dźwięku: organum, polifonia imitacyjna (fuga, a interwał kwinty), forma okresowa, allegro sonatowe.',
+      ],
+      expanded: false,
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  showMore(): void {}
 }
